c9dfb89f5a39ee586acd2c75da6ddf5112b64e00
Ilya Shevelev <30498863+ilyaly@users.noreply.github.com>
2023-12-12 00:40:12 +0100

    Update terrain, improve animation, add sound.

--- index.js
+++ index.js
@@ -10,17 +10,14 @@ import { OutlinePass } from 'three/addons/postprocessing/OutlinePass.js';
 import { OutputPass } from 'three/addons/postprocessing/OutputPass.js';
 import { FXAAShader } from 'three/addons/shaders/FXAAShader.js';
 import { OutlineEffect } from 'three/addons/effects/OutlineEffect.js';
-
 //Loaders
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-//Renderers
-import { CSS2DRenderer, CSS2DObject } from 'three/addons/renderers/CSS2DRenderer.js';
-import { CSS3DRenderer, CSS3DObject } from 'three/addons/renderers/CSS3DRenderer.js';
 //Animation
 import { gsap } from 'https://cdn.skypack.dev/gsap@3.9.0';
 
+
 //Basics
-let scene, camera, renderer, labelRenderer, controls;
+let scene, camera, renderer, controls;
 
 
 let composer, effectFXAA, outlinePass;
@@ -40,25 +37,28 @@ let flight;
 
 let sun;
 
+let isDay = true;
+
 const audioContext = new(window.AudioContext || window.webkitAudioContext)();
 
 let isSceneStartedByUser = false;
 let isSceneLoaded = false;
 
-document.addEventListener("onmousedown", (event) => {
+const basePath = 'https://ilyaly.github.io/enoa-khanid-interactive-map/'
+
+window.addEventListener("onmousedown", (event) => {
     if (flight) {
         flight.kill();
     }
 
 })
 
-document.addEventListener("wheel", (event) => {
+window.addEventListener("wheel", (event) => {
     if (flight) {
         flight.kill();
     }
 
 })
-
 
 init();
 animate();
@@ -71,9 +71,24 @@ playButton.addEventListener('click', function() {
     if (isSceneLoaded) {
         setTimeout(() => {
             const overlay = document.getElementById('overlay');
-            overlay.style.display = 'none';
+            overlay.style.opacity = '0%';
+            overlay.style.pointerEvents = "none";
+            playButton.style.pointerEvents = "none";
+
+            gsap.to(camera.position, {
+                duration: 3,
+                x: 0,
+                y: 2300,
+                z: 0,
+                delay: 0.5,
+                ease: "power3.inOut",
+                onComplete: function() {
+                    controls.maxDistance = 2300;
+                }
+            });
+
             audioContext.resume();
-        }, "3000");
+        }, "1000");
 
     }
 });
@@ -81,23 +96,30 @@ playButton.addEventListener('click', function() {
 const dayNightSwitch = document.getElementById('day-night-switch');
 dayNightSwitch.addEventListener('click', function() {
     if (sun) {
-        if (dayNightSwitch.innerHTML === "☾") {
-            dayNightSwitch.innerHTML = "☉";
-        } else {
+        if (isDay) {
             dayNightSwitch.innerHTML = "☾";
+            gsap.to(sun.rotation, {
+                duration: 7,
+                x: 0,
+                y: 0,
+                z: Math.PI,
+                delay: 0.5,
+                ease: "power3.inOut",
+            });
+            isDay = false;
+        } else {
+            dayNightSwitch.innerHTML = "☉";
+            gsap.to(sun.rotation, {
+                duration: 7,
+                x: 0,
+                y: 0,
+                z: Math.PI / 4,
+                delay: 0.5,
+                ease: "power3.inOut",
+            });
+            isDay = true;
         }
-
-        gsap.to(sun.rotation, {
-            duration: 7,
-            x: 0,
-            y: 0,
-            z: sun.rotation.z + Math.PI,
-            delay: 0.5,
-            ease: "power3.inOut",
-        });
     }
-
-
 });
 
 
@@ -116,7 +138,7 @@ function init() {
 
 
     //Cameras
-    camera.position.set(0, 2300, 0)
+    camera.position.set(0, 4000, 0)
     camera.rotation.set(0, -0.45, 0)
 
     // Controls
@@ -129,8 +151,8 @@ function init() {
     controls.dampingFactor = 0.05;
     controls.screenSpacePanning = false;
     controls.maxPolarAngle = Math.PI / 6;
-    controls.maxDistance = 2300;
-    controls.minDistance = 700;
+    controls.maxDistance = 4000;
+    controls.minDistance = 1000;
     controls.autoRotateSpeed = 0.5;
 
 
@@ -146,26 +168,17 @@ function init() {
         camera.position.sub(_v);
     })
 
-    //Label 
-    labelRenderer = new CSS3DRenderer();
-    labelRenderer.setSize(window.innerWidth, window.innerHeight);
-    labelRenderer.domElement.id = 'label-renderer';
-    labelRenderer.domElement.style.position = 'absolute';
-    labelRenderer.domElement.style.top = '0px';
-    labelRenderer.domElement.style.pointerEvents = 'none';
-    labelRenderer.domElement.style.zIndex = '1'
-    document.body.appendChild(labelRenderer.domElement);
 
     // Light
-    scene.add(new THREE.AmbientLight(0x5c9fbc, 1));
+    scene.add(new THREE.AmbientLight(0x5c9fbc, 0.5));
     //const hL = scene.add(new THREE.HemisphereLight(0xffffbb, 0x080820, 1));
     //hL.position.set(0, 0, 0)
-    const d = 100;
-   
+    const d = 10000;
+
     sun = new THREE.Group();
 
     let light_2 = new THREE.DirectionalLight(0xFCE570, 1);
-    /*
+
     light_2.position.set(0, 1000, 0);
     light_2.castShadow = true;
     light_2.shadow.mapSize.width = 1024;
@@ -176,14 +189,14 @@ function init() {
     light_2.shadow.camera.right = d;
     light_2.shadow.camera.top = d;
     light_2.shadow.camera.bottom = -d;
-    light_2.shadow.camera.far = 2000;
-    */
+    light_2.shadow.camera.far = 5000;
+
     sun.add(light_2);
-    sun.rotation.z + Math.PI / 4
     scene.add(sun);
+    sun.rotation.z = Math.PI / 4;
 
     //Fog
-    scene.fog = new THREE.FogExp2(0x000000, 0.00015);
+    //scene.fog = new THREE.FogExp2(0x000000, 0.00015);
     //scene.fog = new THREE.Fog(0xcccccc, 2000, 2600);
 
     //Audio
@@ -193,7 +206,7 @@ function init() {
 
 
     //Clouds
-    cloudsTexture = new THREE.TextureLoader().load('https://ilyaly.github.io/enoa-khanid-interactive-map/public/images/fx_cloudalpha05.png');
+    cloudsTexture = new THREE.TextureLoader().load(`${basePath}public/images/fx_cloudalpha05.png`);
     cloudsTexture.wrapS = THREE.RepeatWrapping;
     cloudsTexture.wrapT = THREE.RepeatWrapping;
     const planeGeometry = new THREE.PlaneGeometry(10000, 10000, 10, 10);
@@ -202,6 +215,9 @@ function init() {
     cloudsPlane.position.set(0, 1000, 0);
     cloudsPlane.rotation.x = -1.57079633;
     scene.add(cloudsPlane);
+
+
+
 
     // postprocessing
 
@@ -217,14 +233,6 @@ function init() {
     composer.addPass(outlinePass);
 
 
-    /*
-    const bokehPass = new BokehPass(scene, camera, {
-        focus: 130,
-        aperture: 3.1,
-        maxblur: 0.005
-    });
-    composer.addPass( bokehPass );
-    */
 
     const outputPass = new OutputPass();
     composer.addPass(outputPass);
@@ -241,8 +249,6 @@ function init() {
 
     function onPointerMove(event) {
 
-        //if (event.isPrimary === false) return;
-
         mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
         mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
 
@@ -250,44 +256,43 @@ function init() {
         const pickedObject = checkIntersection()
         if (pickedObject) {
             mouseOverObject = pickedObject;
-            outlinePass.selectedObjects = [pickedObject];
+            outlinePass.selectedObjects = [mouseOverObject];
         } else {
             mouseOverObject = null;
             outlinePass.selectedObjects = [];
         }
-
-
     }
-
 
     function onMouseClick(event) {
         mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
         mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
 
-        raycaster.setFromCamera(mouse, camera);
-        const i = raycaster.intersectObject(scene, true);
-        console.log(i[0].point)
-
+        if (selectedObject) {
+            selectedObject.getObjectByProperty("type", "Audio").stop();
+        }
         selectedObject = null;
         const pickedObject = checkIntersection()
         if (pickedObject) {
-            setSideBar(pickedObject.userData);
+
             selectedObject = pickedObject;
-            outlinePass.selectedObjects = [selectedObject];
+            setSideBar(selectedObject.userData);
+            selectedObject.getObjectByProperty("type", "Audio").play();
+
             zoomInTimeline(
                 pickedObject.position.x,
                 pickedObject.position.y,
                 pickedObject.position.z,
-                700
+                1200
             );
             controls.autoRotate = true;
         } else {
             resetSideBar();
             selectedObject = null;
-            outlinePass.selectedObjects = [];
             controls.autoRotate = false;
+            if (selectedObject) {
+                selectedObject.getObjectByProperty("type", "Audio").stop();    
+            }
         }
-
     }
 
 
@@ -306,35 +311,29 @@ function init() {
             }
         }
         return null
-
     };
 
     const zoomInTimeline = (x, y, z, zoomOutFactor = 10) => {
         flight = gsap
-            .timeline({ defaults: { duration: 5.5, ease: "expo.out" } })
+            .timeline({ defaults: { duration: 5.5, ease: "expoScale(0.5,7,none)" } })
             .to(controls.target, { x, y, z })
             .to(camera.position, { x, y, z: z + zoomOutFactor }, 0)
-        //.to(group.rotation, { x: 0, y: 0 }, 0);
     };
 
 
     //Loaders
     const loadingManager = new THREE.LoadingManager();
     loadingManager.onLoad = function() {
+        console.log("Scene loaded")
         isSceneLoaded = true;
-        if (isSceneStartedByUser) {
-            setTimeout(() => {
-                const overlay = document.getElementById('overlay');
-                overlay.style.display = 'none';
-                audioContext.resume();
-            }, "3000");
-        }
+        const overlay = document.getElementById('overlay');
+        overlay.style.opacity = '50%';
     };
     const modelLoader = new GLTFLoader(loadingManager);
     const audioLoader = new THREE.AudioLoader(loadingManager);
 
     //Load data
-    const request = new Request("https://ilyaly.github.io/enoa-khanid-interactive-map/public/sites.json");
+    const request = new Request(`${basePath}public/sites.json`);
     fetch(request)
         .then((response) => response.json())
 
@@ -352,10 +351,6 @@ function init() {
         })
 
         .catch(console.error);
-
-
-
-
 }
 
 
@@ -405,7 +400,7 @@ function loadSite(
     //Load model
     if (site.model) {
         let model;
-        modelLoader.load(`https://ilyaly.github.io/enoa-khanid-interactive-map/public/models/${site.model}`, (gltf) => {
+        modelLoader.load(`${basePath}public/models/${site.model}`, (gltf) => {
             model = gltf.scene;
             /*
             const toonMaterial = new THREE.MeshToonMaterial();
@@ -431,40 +426,18 @@ function loadSite(
     if (site.sound) {
         const sound = new THREE.PositionalAudio(audioListener);
 
-        audioLoader.load(`https://ilyaly.github.io/enoa-khanid-interactive-map/public/sounds/${site.sound}`, function(buffer) {
+        audioLoader.load(`${basePath}public/sounds/${site.sound}`, function(buffer) {
             sound.setBuffer(buffer);
             sound.setLoop(true);
-            sound.setRolloffFactor(1);
-            sound.setDistanceModel("linear")
-            //sound.setRefDistance(250); 
-            sound.setMaxDistance(550);
-            sound.setVolume(2);
-            sound.play();
+            sound.setRolloffFactor(4);
+            //sound.setDistanceModel("linear")
+            sound.setRefDistance(500); 
+            //sound.setMaxDistance(1500);
+            sound.setVolume(0.5);
+            //sound.play();
             group.add(sound);
         });
     }
-
-    /*
-    // Set label
-    if (site.name && site.type !==
-        "terrain") {
-
-        const labelContainer = document.createElement('div');
-        labelContainer.className = 'label';
-        labelContainer.textContent = site.name;
-        const label = new CSS3DObject(labelContainer);
-        /*
-        label.position.set(
-            site.position[0],
-            site.position[1],
-            site.position[2]
-        )
-        
-        //label.position.y += 50;
-
-        group.add(label);
-    }
-    */
 
     //Add site to scene
     scene.add(group);
@@ -474,8 +447,6 @@ function loadSite(
         siteIds.push(group.id);
     }
 }
-
-
 
 
 function onWindowResize() {
@@ -488,7 +459,6 @@ function onWindowResize() {
 
     renderer.setSize(width, height);
     composer.setSize(width, height);
-    labelRenderer.setSize(width, height);
 
     effectFXAA.uniforms['resolution'].value.set(1 / window.innerWidth, 1 / window.innerHeight);
 
@@ -510,14 +480,9 @@ function animate() {
 
 function render() {
     //renderer.render(scene, camera);
-    labelRenderer.render(scene, camera);
     composer.render();
 
-
-
 }
-
-
 
 const setSideBar = (data) => {
     const sidebar = document.getElementById('sidebar');
